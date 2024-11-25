138d8129e52b6df67a857330c0e1928f23c1ea8b
Ludovic Mermod <ludovic.mermod@epfl.ch>
2024-11-25 11:03:24 +0100

    feat: add get-tasks module

---
+++ get-tasks/get-tasks.c
@@ -0,0 +1,26 @@
+#include "linux/pid.h"
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
+#include <linux/sched/task_stack.h>
+#include <linux/thread_info.h>
+
+MODULE_LICENSE("GPL");
+
+static int hello_init(void) {
+  printk(KERN_ALERT "Hello, world\n");
+
+  struct task_struct *task = find_task_by_vpid((pid_t)163);
+  printk(KERN_INFO "Process: %ld [PID = %d]\n", task->thread.sp, task->pid);
+  struct pt_regs *regs = task_pt_regs(task);
+
+  printk(KERN_INFO "RIP %p\n", (void *)regs->ip);
+
+  return 0;
+}
+static void hello_exit(void) { printk(KERN_ALERT "Goodbye, cruel world\n"); }
+
+module_init(hello_init);
+module_exit(hello_exit);
